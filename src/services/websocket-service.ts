f7a342213e430897ab64834c855e964a38548ad5
Lelberto <jsurieux@gmail.com>
2021-05-27 18:52:50 +0200

    Add join event

--- src/services/websocket-service.ts
+++ src/services/websocket-service.ts
@@ -1,4 +1,5 @@
 import { Server, Socket } from 'socket.io';
+import { Player } from '../logic/player';
 import Service from './service';
 import ServiceContainer from './service-container';
 
@@ -62,7 +63,65 @@ export default class WebsocketService extends Service {
           this.logger.info(`Websocket disconnected : ${socket.handshake.address}`);
       });
 
-      // Websocket logic here
+      socket.on(Event.JOIN, async ({ code, userId }: JoinClientToServerEvent) => {
+        const game = this.container.game.games.find(game => game.code === code);
+        if (game == null) {
+          socket.emit(Event.ERROR, {
+            error: Error.GAME_NOT_FOUND,
+            description: `Game not found with code ${code}`
+          } as ErrorServerToClientEvent);
+        }
+        const user = await this.db.users.findById(userId);
+        if (user == null) {
+          socket.emit(Event.ERROR, {
+            error: Error.USER_NOT_FOUND,
+            description: `User not found with id ${userId}`
+          } as ErrorServerToClientEvent);
+        }
+        if (!game.players.map(player => player.id).includes(userId, 1)) {
+          game.players.push(new Player(user));
+        }
+        socket.emit(Event.JOIN, { gameId: game.id } as JoinServerToClientEvent);
+      });
     });
   }
+}
+
+/**
+ * Websocket events enum.
+ */
+enum Event {
+  JOIN = 'join',
+  ERROR = 'error'
+}
+
+/**
+ * Websocket errors enum.
+ */
+enum Error {
+  GAME_NOT_FOUND = 0,
+  USER_NOT_FOUND = 0
+}
+
+/**
+ * Event when player is joining a game.
+ */
+interface JoinClientToServerEvent {
+  code: string;
+  userId: string;
+}
+
+/**
+ * Event when server accepts joining game.
+ */
+interface JoinServerToClientEvent {
+  gameId: string;
+}
+
+/**
+ * Event when an error occurs.
+ */
+interface ErrorServerToClientEvent {
+  error: Error;
+  description?: string;
 }
