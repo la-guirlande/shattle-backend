474a747922edbbad703ddb0ec8ffde684193616d
Michel Rodrigues <michel.rodrigues38150@gmail.com>
2021-06-23 13:07:14 +0200

    comment

--- src/services/notification-service.ts
+++ src/services/notification-service.ts
@@ -1,13 +1,13 @@
-import Service from './service';
-import webpush from 'web-push';
+//import Service from './service';
+//import webpush from 'web-push';
 /**
  * Notification service class.
  * 
  * This service manages Shattle notifications.
  */
-export default class NotificationService extends Service {
+/*export default class NotificationService extends Service {
 
     public readonly notifications:Notification[];
   
 }
-webpush.setVapidDetails(process.env.WEB_PUSH_CONTACT, process.env.PUBLIC_VAPID_KEY, process.env.PRIVATE_VAPID_KEY)
+webpush.setVapidDetails(process.env.WEB_PUSH_CONTACT, process.env.PUBLIC_VAPID_KEY, process.env.PRIVATE_VAPID_KEY)*/
